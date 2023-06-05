96f686673df49ec2ba99730c2ea9f67513dcb4e6
Mary-clouds <marynevermann@gmail.com>
2023-06-05 13:20:29 +0200

    add site link in footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        this project was coded by mary nevermann and is{" "}
+        <a href="https://weatherapp-react-poject.netlify.app" target="blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
